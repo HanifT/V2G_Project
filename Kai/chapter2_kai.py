1a38b90633253ffbd641c9b33e349fd254c3a66d
Hanif Tayarani <147196834+HanifT@users.noreply.github.com>
2024-07-01 11:59:59 -0700

    Delete Kai directory

--- Kai/chapter2_kai.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from kai_read import (read_clean_kai, extract_datetime_components, rename_col)
-
-data = read_clean_kai()
-data = extract_datetime_components(data)
-data = rename_col(data)
-
-
